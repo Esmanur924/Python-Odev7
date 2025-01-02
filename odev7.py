58ce1c14495b6b50bb525ada7f45e6a589a7af0a
Esmanur924 <esmanur.akpunar@std.yildiz.edu.tr>
2025-01-02 17:51:16 +0300

    Python-Odev7

---
+++ odev7.py
@@ -0,0 +1,33 @@
+"""
+    module1 (db)      : urunler
+    module2 (methods) : urunEkle(), urunGuncelle(), urunleriGetir()
+    module3 (app)     :
+
+        yeni ürün ekleme => urunEkle("iphone 15", 60000)
+        ürün güncelle    => urunGuncelle(1, "iphone 15 pro", 80000)
+        ürünleri listele => urunleriGetir() 
+"""
+
+urunler=[]
+
+def urunEkle(urunAdi, fiyat):
+    urun={"id": len(urunler) + 1,"urunAdi": urunAdi, "fiyat": fiyat}
+    urunler.append(urun)
+    return urun 
+
+def urunGuncelle(urunId, yeniUrunAd, yeniFiyat):
+    for urun in urunler:
+        if urun["id"]== urunId:
+            urun["urunAdi"]=yeniUrunAd
+            urun["fiyat"]= yeniFiyat
+            return urun
+    return None
+
+def urunleriGetir():
+    return urunler
+
+urunEkle("Iphone 15", 60000)
+urunGuncelle(1, "Iphone 15 Pro", 80000)
+liste=urunleriGetir()
+for urun in liste:
+    print(f"ID: {urun['id']}, Ürün:{urun['urunAdi']}, Fiyat: {urun['fiyat']}")
